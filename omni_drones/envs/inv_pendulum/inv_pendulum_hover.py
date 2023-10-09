fe0d5919dfc01afa839ceeb2829a1d63391a0eef
zhangruize <2290321870@qq.com>
2023-10-09 22:46:20 +0800

    update docs of single-agent tasks

--- omni_drones/envs/inv_pendulum/inv_pendulum_hover.py
+++ omni_drones/envs/inv_pendulum/inv_pendulum_hover.py
@@ -45,37 +45,45 @@ class InvPendulumHover(IsaacEnv):
     We refer the the ball at the end of pendulum as *payload*. The goal for the agent 
     is to keep balance while maintaining its position around a target position.
 
-    Observation
-    -----------
+    ## Observation
     - `drone_payload_rpos` (3): The position of the drone relative to the payload's position.
-    - `root_state` (16 + num_rotors): The basic information of the drone (except its position), 
+    - `root_state` (19 + num_rotors): The basic information of the drone, 
       containing its rotation (in quaternion), velocities (linear and angular), 
       heading and up vectors, and the current throttle.
     - `target_payload_rpos` (3): The position of the reference relative to the payload's position.
     - `payload_vel` (6): The linear and angular velocities of the payload.
-    - *time_encoding*: 
+    - `time_encoding` (optional): The time encoding, which is a 4-dimensional
+      vector encoding the current progress of the episode.
+ 
+    ## Reward
 
-    Reward 
-    ------
-    - pos: 
-    - bar_up:
-    - effort:
-    - spin:
-    - swing:
+    - `pos`: Reward for maintaining the position of the payload around the target position.
+    - `bar_up`: Reward for keeping the bar up.
+    - `effort`: Reward computed from the effort of the drone to optimize the
+      energy consumption.
+    - `spin`: Reward computed from the spin of the drone to discourage spinning.
+    - `swing`: Reward computed from the swing of the payload to discourage swinging.
 
-    The total reward is 
+    The total reward is computed as follows:
 
-    .. math:: 
-    
+    ```{math}
         r = r_\text{bar_up} + r_\text{pos} + r_\text{bar_up} * (r_\text{spin} + r_\text{swing}) + r_\text{effort}
+    ```
 
-    Episode End
-    -----------
-    - Termination: 
+    ## Episode End
+    The episode ends when the bar falls beyond a certain angle, or when the
+    drone gets too close to the ground, or when the distance between the payload 
+    and the target exceeds a threshold, or when the maximum episode length
+    is reached.
 
-    Config
-    ------
-    - `bar_length`: The length of the pendulum's bar (or pole).
+    ## Config
+
+    | Parameter               | Type  | Default       | Description |
+    |-------------------------|-------|---------------|-------------|
+    | `drone_model`           | str   | "hummingbird" | Specifies the model of the drone being used in the environment. |
+    | `bar_length`            | float | 1.0           | Length of the pendulum's bar. |
+    | `reward_distance_scale` | float | 1.2           | Scales the reward based on `target_payload_rpos`. |
+    | `time_encoding`         | bool  | True          | Indicates whether to include time encoding in the observation space. If set to True, a 4-dimensional vector encoding the current progress of the episode is included in the observation. If set to False, this feature is not included. |
 
     """
     def __init__(self, cfg, headless):
--- omni_drones/envs/inv_pendulum/inv_pendulum_track.py
+++ omni_drones/envs/inv_pendulum/inv_pendulum_track.py
@@ -43,6 +43,50 @@ from .utils import create_pendulum
 
 
 class InvPendulumTrack(IsaacEnv):
+    r"""
+    An intermidiate control task where a drone-based inverted pendulum system
+    need to track a reference lemniscate trajectory for the payload.
+
+    ## Observation
+    
+    - `drone_payload_rpos` (3): The position of the drone relative to the payload's position.
+    - `root_state` (16 + `num_rotors`): The basic information of the drone (except its position), 
+      containing its rotation (in quaternion), velocities (linear and angular), 
+      heading and up vectors, and the current throttle.
+    - `target_payload_rpos` (3 * `future_traj_steps`): The position of the reference relative to the payload's position.
+    - `payload_vel` (6): The linear and angular velocities of the payload.
+    - `time_encoding` (optional): The time encoding, which is a 4-dimensional
+      vector encoding the current progress of the episode.
+
+    ## Reward
+    
+    - `pos`: Reward for tracking the trajectory based on how close the drone's payload is to the target position.
+    - `effort`: Reward computed from the effort of the drone to optimize the
+      energy consumption.
+    - `action_smoothness`: Reward that encourages smoother drone actions, computed based on the throttle difference of the drone.
+
+    The total reward is computed as follows:
+    ```{math}
+        r = r_\text{pos} + r_\text{effort} + r_\text{action_smoothness}
+    ```
+
+    ## Episode End
+    The episode ends when the bar falls beyond a certain angle, or when the
+    drone gets too close to the ground, or when the distance between the payload 
+    and the target exceeds a threshold, or when the maximum episode length
+    is reached.
+    
+    ## Config
+
+    | Parameter               | Type  | Default   | Description |
+    |-------------------------|-------|-----------|-------------|
+    | `drone_model`           | str   | "firefly" | Specifies the model of the drone being used in the environment. |
+    | `reset_thres`           | float | 0.5       | Threshold for the distance between the payload and its target, upon exceeding which the episode will be reset. |
+    | `future_traj_steps`     | int   | 4         | Number of future trajectory steps the drone needs to predict. |
+    | `reward_distance_scale` | float | 1.2       | Scales the reward based on the distance between the payload and its target. |
+    | `time_encoding`         | bool  | True      | Indicates whether to include time encoding in the observation space. If set to True, a 4-dimensional vector encoding the current progress of the episode is included in the observation. If set to False, this feature is not included. |
+
+    """
     def __init__(self, cfg, headless):
         self.reset_thres = cfg.task.reset_thres
         self.reward_effort_weight = cfg.task.reward_effort_weight
