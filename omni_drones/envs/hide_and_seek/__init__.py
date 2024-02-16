1da2bbceb040e293d9f5583f91be5895d73c93a7
jiayu-ch15 <jiayu-ch15@mails.tsinghua.edu.cn>
2024-02-16 15:11:34 +0800

    add circle

--- omni_drones/envs/hide_and_seek/__init__.py
+++ omni_drones/envs/hide_and_seek/__init__.py
@@ -1,3 +1,4 @@
 from .hideandseek import HideAndSeek
 from .hideandseek_static import HideAndSeek_static
 from .hideandseek_sparse import HideAndSeek_sparse
+from .hideandseek_circle import HideAndSeek_circle
