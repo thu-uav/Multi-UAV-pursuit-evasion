f1a3db581cd7d4e048ddffdae8b8725b635b928b
jiayu-ch15 <jiayu-ch19@mails.tsinghua.edu.cn>
2024-03-09 22:27:37 +0800

    update

--- omni_drones/envs/hide_and_seek/__init__.py
+++ omni_drones/envs/hide_and_seek/__init__.py
@@ -3,5 +3,6 @@ from .hideandseek_circle import HideAndSeek_circle
 from .hideandseek_circle_static_ued import HideAndSeek_circle_static_UED
 from .hideandseek_circle_static_ued_cl import HideAndSeek_circle_static_UED_cl
 from .hideandseek_circle_static_ued_cl_v2 import HideAndSeek_circle_static_UED_cl_v2
+from .hideandseek_circle_static_ued_cl_v3 import HideAndSeek_circle_static_UED_cl_v3
 from .hideandseek_circle_eval import HideAndSeek_circle_eval
 from .hideandseek_multiprey import HideAndSeek_multiprey
